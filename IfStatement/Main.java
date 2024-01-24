5621769d2ba471f9ce5b1ad882ecfa94e536a788
TomatoFT <20520926@gm.uit.edu.vn>
2024-01-24 23:13:41 +0700

    Add the if statemment and Modified some change

---
+++ IfStatement/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type your age: ");
+        int age = scanner.nextInt();
+
+        if(age < 12) {
+            System.out.println("You are still the kid");
+        }
+        else {
+            if(age > 18) {
+                System.out.println("You are adult");
+            }
+            else {
+                System.out.println("You are teenager");
+            }
+        }
+        scanner.close();
+    }
+}
