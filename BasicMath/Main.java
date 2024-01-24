87c6d6650a82e90b48f90a91901c95ddb6a8d830
TomatoFT <20520926@gm.uit.edu.vn>
2024-01-24 19:45:34 +0700

    Remove the class file

---
+++ BasicMath/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x: ");
+        int x = scanner.nextInt();
+        System.out.println("Next y: ");
+        int y = scanner.nextInt();
+
+        System.out.println(Math.sqrt((x*x)+(y*y)));
+
+    }
+}
---
+++ BasicMath/Practice.java
@@ -0,0 +1,28 @@
+
+public class Practice {
+    private static int getMin(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    private static int getMax(int a, int b) {
+        return Math.max(a ,b);
+    }
+
+    private static int getAbs(int a) {
+        return Math.abs(a);
+    }
+    private static double getSqrt(int a) {
+        return Math.sqrt(a);
+    }
+    private static double getRound(double a) {
+        return Math.round(a);
+    }
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        System.out.println(getMin(a,b));
+        System.out.println(getMax(a,b));
+        System.out.println(getSqrt(4));
+    }
+}
